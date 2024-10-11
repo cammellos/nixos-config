4f80eb875a58135f0a53a0b12ae73a99feb4a48e
Andrea Maria Piana <andrea.maria.piana@gmail.com>
2024-10-11 12:32:45 +0100

    feat: upgrade flake

--- users/cammellos/pkgs/obs.nix
+++ users/cammellos/pkgs/obs.nix
@@ -1,10 +1,10 @@
-{ config, pkgs, ...}:
+{ config, pkgs, ... }:
 {
   home-manager.users.cammellos.programs.obs-studio = {
-      enable = true;
-      plugins = with pkgs.obs-studio-plugins; [
-        wlrobs
-        obs-pipewire-audio-capture
-      ];
-    };
+    enable = true;
+    plugins = with pkgs.obs-studio-plugins; [
+      wlrobs
+      obs-pipewire-audio-capture
+    ];
+  };
 }
