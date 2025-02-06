1a10b9e9b7e1f874ba192550638a53711ff43929
Andrea Maria Piana <andreamaria.piana@Remote-Andrea-Maria-Piana.local>
2025-02-06 11:43:52 +0000

    feat: add kubectl

--- flake.nix
+++ flake.nix
@@ -3,13 +3,17 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     nix-darwin.url = "github:LnL7/nix-darwin/nix-darwin-24.11";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+
     home-manager.url = "github:nix-community/home-manager";
+
+    nixpkgs-kubectl.url = "github:nixos/nixpkgs/10b813040df67c4039086db0f6eaf65c536886c6";
   };
 
   outputs =
-    { self, nix-darwin, nixpkgs, home-manager, ... }@inputs:
+    { self, nix-darwin, nixpkgs, home-manager, nixpkgs-kubectl, ... }@inputs:
     {
-      darwinConfigurations.moosel = nix-darwin.lib.darwinSystem {
+      darwinConfigurations.moosel = nix-darwin.lib.darwinSystem rec {
+        system = "aarch64-darwin";
         modules = [
           ./mac-os-configuration.nix
           home-manager.darwinModules.home-manager
@@ -26,6 +30,13 @@
             ];
           }
         ];
+        specialArgs = {
+          pkgs-kubectl = import nixpkgs-kubectl {
+            inherit system;
+            config.allowUnfree = true;
+          };
+        };
+
       };
       nixosConfigurations.moosel = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
--- users/andreamaria.piana/default.nix
+++ users/andreamaria.piana/default.nix
@@ -1,11 +1,18 @@
-{ config, pkgs, ...}:
+{ config, pkgs, pkgs-kubectl, ...}:
 {
-  users.users."andreamaria.piana".packages = with pkgs;
-  [
-    silver-searcher
+  users.users."andreamaria.piana".packages =  [
+    pkgs-kubectl.kubectl
+    pkgs.silver-searcher
+    pkgs.awscli
+
   ];
   imports = [
     ./home.nix
+    (import ../../pkgs/tmux {
+      user = "andreamaria.piana";
+      inherit config;
+      inherit pkgs;
+    })
     (import ../../pkgs/direnv.nix {
       user = "andreamaria.piana";
       inherit config;
