92d1fcad8d70e8f038a5b7d5f3fb0afe040f15c7
Andrea Maria Piana <andrea.maria.piana@gmail.com>
2025-06-17 19:35:37 +0100

    nvim enter in insert mode in terminal

--- pkgs/neovim/config/lua/bindings.lua
+++ pkgs/neovim/config/lua/bindings.lua
@@ -1,78 +1,85 @@
-vim.api.nvim_set_keymap('n', ';', ':', { noremap = true, silent = false})
-vim.api.nvim_set_keymap('n', ':', ';', { noremap = true, silent = false})
-vim.api.nvim_set_keymap('i', 'uu', '<Esc>', { noremap = true, silent = false })
-vim.api.nvim_set_keymap('i', 'UUUU', '<Nop>', { noremap = true, silent = true })
+local opts = { noremap = true, silent = true }
+
+-- Top-level keymaps
+vim.keymap.set('n', ';', ':', { noremap = true, silent = false })
+vim.keymap.set('n', ':', ';', { noremap = true, silent = false })
+vim.keymap.set('i', 'uu', '<Esc>', { noremap = true, silent = false })
+vim.keymap.set('i', 'UUUU', '<Nop>', opts)
 
 -- Define the Keyboard function
 local function WorkmanKeyboard()
   -- Visual mode remappings
-  vim.api.nvim_set_keymap('v', 'l', 'o', { noremap = true, silent = true })
-  vim.api.nvim_set_keymap('v', 'o', 'l', { noremap = true, silent = true })
-  vim.api.nvim_set_keymap('v', 'L', 'O', { noremap = true, silent = true })
-  vim.api.nvim_set_keymap('v', 'O', 'L', { noremap = true, silent = true })
+  vim.keymap.set('v', 'l', 'o', opts)
+  vim.keymap.set('v', 'o', 'l', opts)
+  vim.keymap.set('v', 'L', 'O', opts)
+  vim.keymap.set('v', 'O', 'L', opts)
 
-  -- Search (N)ext -> (J)ump
-  vim.api.nvim_set_keymap('v', 'j', 'n', { noremap = true, silent = true })
-  vim.api.nvim_set_keymap('v', 'n', 'j', { noremap = true, silent = true })
-  vim.api.nvim_set_keymap('v', 'J', 'N', { noremap = true, silent = true })
-  vim.api.nvim_set_keymap('v', 'N', 'J', { noremap = true, silent = true })
+  vim.keymap.set('v', 'j', 'n', opts)
+  vim.keymap.set('v', 'n', 'j', opts)
+  vim.keymap.set('v', 'J', 'N', opts)
+  vim.keymap.set('v', 'N', 'J', opts)
 
-  -- (E)nd of word -> brea(K) of word
-  vim.api.nvim_set_keymap('v', 'k', 'e', { noremap = true, silent = true })
-  vim.api.nvim_set_keymap('v', 'e', 'k', { noremap = true, silent = true })
-  vim.api.nvim_set_keymap('v', 'K', 'E', { noremap = true, silent = true })
-  vim.api.nvim_set_keymap('v', 'E', '', { noremap = true, silent = true })
+  vim.keymap.set('v', 'k', 'e', opts)
+  vim.keymap.set('v', 'e', 'k', opts)
+  vim.keymap.set('v', 'K', 'E', opts)
+  vim.keymap.set('v', 'E', '', opts)
 
-  -- Yank to Haul
-  vim.api.nvim_set_keymap('v', 'h', 'y', { noremap = true, silent = true })
-  vim.api.nvim_set_keymap('o', 'h', 'y', { noremap = true, silent = true })
-  vim.api.nvim_set_keymap('v', 'y', 'h', { noremap = true, silent = true })
-  vim.api.nvim_set_keymap('v', 'H', 'Y', { noremap = true, silent = true })
-  vim.api.nvim_set_keymap('v', 'Y', 'H', { noremap = true, silent = true })
+  vim.keymap.set('v', 'h', 'y', opts)
+  vim.keymap.set('v', 'y', 'h', opts)
+  vim.keymap.set('v', 'H', 'Y', opts)
+  vim.keymap.set('v', 'Y', 'H', opts)
+  vim.keymap.set('o', 'h', 'y', opts)
 
   -- Normal mode remappings
-  vim.api.nvim_set_keymap('n', 'l', 'o', { noremap = true, silent = true })
-  vim.api.nvim_set_keymap('n', 'o', 'l', { noremap = true, silent = true })
-  vim.api.nvim_set_keymap('n', 'L', 'O', { noremap = true, silent = true })
-  vim.api.nvim_set_keymap('n', 'O', 'L', { noremap = true, silent = true })
+  vim.keymap.set('n', 'l', 'o', opts)
+  vim.keymap.set('n', 'o', 'l', opts)
+  vim.keymap.set('n', 'L', 'O', opts)
+  vim.keymap.set('n', 'O', 'L', opts)
 
-  -- Search (N)ext -> (J)ump
-  vim.api.nvim_set_keymap('n', 'j', 'n', { noremap = true, silent = true })
-  vim.api.nvim_set_keymap('n', 'n', 'j', { noremap = true, silent = true })
-  vim.api.nvim_set_keymap('n', 'J', 'N', { noremap = true, silent = true })
-  vim.api.nvim_set_keymap('n', 'N', 'J', { noremap = true, silent = true })
+  vim.keymap.set('n', 'j', 'n', opts)
+  vim.keymap.set('n', 'n', 'j', opts)
+  vim.keymap.set('n', 'J', 'N', opts)
+  vim.keymap.set('n', 'N', 'J', opts)
 
-  -- (E)nd of word -> brea(K) of word
-  vim.api.nvim_set_keymap('n', 'k', 'e', { noremap = true, silent = true })
-  vim.api.nvim_set_keymap('n', 'e', 'k', { noremap = true, silent = true })
-  vim.api.nvim_set_keymap('n', 'K', 'E', { noremap = true, silent = true })
-  vim.api.nvim_set_keymap('n', 'E', '', { noremap = true, silent = true })
+  vim.keymap.set('n', 'k', 'e', opts)
+  vim.keymap.set('n', 'e', 'k', opts)
+  vim.keymap.set('n', 'K', 'E', opts)
+  vim.keymap.set('n', 'E', '', opts)
 
-  -- Yank to Haul
-  vim.api.nvim_set_keymap('n', 'y', 'h', { noremap = true, silent = true })
-  vim.api.nvim_set_keymap('n', 'H', 'Y', { noremap = true, silent = true })
-  vim.api.nvim_set_keymap('n', 'Y', 'H', { noremap = true, silent = true })
+  vim.keymap.set('n', 'y', 'h', opts)
+  vim.keymap.set('n', 'H', 'Y', opts)
+  vim.keymap.set('n', 'Y', 'H', opts)
 
-  -- Split mapping
-  vim.api.nvim_set_keymap('n', ',a', '<C-w>v', { noremap = true, silent = true })
-  vim.api.nvim_set_keymap('n', ',d', '<C-w>h', { noremap = true, silent = true })
-  vim.api.nvim_set_keymap('n', ',r', '<C-w>l', { noremap = true, silent = true })
+  -- Window splits
+  vim.keymap.set('n', ',a', '<C-w>v', opts)
+  vim.keymap.set('n', ',d', '<C-w>h', opts)
+  vim.keymap.set('n', ',r', '<C-w>l', opts)
 
-  -- Tab remapping
-  vim.api.nvim_set_keymap('n', ',t', ':tabnew<CR>', { noremap = true, silent = true })
-  vim.api.nvim_set_keymap('i', ',t', '<Esc>:tabnew<CR>', { noremap = true, silent = true })
-  vim.api.nvim_set_keymap('t', ',t', '<C-\\><C-N>:tabnew<CR><C-\\><C-N>', { noremap = true, silent = true })
+  -- Tabs
+  vim.keymap.set('n', ',t', ':tabnew<CR>', opts)
+  vim.keymap.set('i', ',t', '<Esc>:tabnew<CR>', opts)
+  vim.keymap.set('t', ',t', '<C-\\><C-N>:tabnew<CR><C-\\><C-N>', opts)
 
-  vim.api.nvim_set_keymap('n', ',o', ':tabnext<CR>', { noremap = true, silent = true })
-  vim.api.nvim_set_keymap('n', ',y', ':tabprevious<CR>', { noremap = true, silent = true })
+  -- Tab navigation
+  vim.keymap.set({ 'n', 'i', 't' }, ',o', function()
+    vim.cmd('tabnext')
+    if vim.bo.buftype == 'terminal' then
+      vim.cmd('startinsert')
+    end
+  end, opts)
 
-  -- terminal
-  vim.api.nvim_set_keymap('n', ',s', ':term<CR>', { noremap = true, silent = true })
-  -- use esc to leave terminal mode
-  vim.api.nvim_set_keymap('t', '<Esc>', '<C-\\><C-n>', { noremap = true, silent = true })
-vim.keymap.set("t", ",o", "<C-\\><C-N>:tabnext<CR>", opts)
-vim.keymap.set("t", ",y", "<C-\\><C-N>:tabprevious<CR>", opts)
+  vim.keymap.set({ 'n', 'i', 't' }, ',y', function()
+    vim.cmd('tabprevious')
+    if vim.bo.buftype == 'terminal' then
+      vim.cmd('startinsert')
+    end
+  end, opts)
 
+  -- Terminal: open terminal
+  vim.keymap.set('n', ',s', ':term<CR>', opts)
+
+  -- Escape from terminal mode
+  vim.keymap.set('t', '<Esc>', '<C-\\><C-n>', opts)
 end
 
 WorkmanKeyboard()
